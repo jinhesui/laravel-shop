a2fc4bb29e9beb6d94233b3699b99d2d8e4285e0
jinhesui <jinhesui@163.com>
2018-11-10 14:25:43 +0000

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,10 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-    // 开始
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+        // 删掉这里面的内容
     });
-    // 结束
 });
