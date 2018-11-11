cb9bb497af2ba400e0f3c7cc25e46d2f06c2ee0e
jinhesui <jinhesui@163.com>
2018-11-11 06:31:53 +0000

    管理后台 - 新增、编辑商品

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -1,24 +1,18 @@
 <?php
-
 namespace App\Admin\Controllers;
-
 use App\Models\Product;
-use App\Http\Controllers\Controller;
-use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
-use Encore\Admin\Layout\Content;
-use Encore\Admin\Show;
 use Encore\Admin\Facades\Admin;
-
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
 class ProductsController extends Controller
 {
-    use HasResourceActions;
-
+    use ModelForm;
     /**
      * Index interface.
      *
-     * @param Content $content
      * @return Content
      */
     public function index()
@@ -28,51 +22,31 @@ class ProductsController extends Controller
             $content->body($this->grid());
         });
     }
-
-    /**
-     * Show interface.
-     *
-     * @param mixed   $id
-     * @param Content $content
-     * @return Content
-     */
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
-    }
-
     /**
      * Edit interface.
      *
-     * @param mixed   $id
-     * @param Content $content
+     * @param $id
      * @return Content
      */
-    public function edit($id, Content $content)
+    public function edit($id)
     {
-        return $content
-            ->header('Edit')
-            ->description('description')
-            ->body($this->form()->edit($id));
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header('编辑商品');
+            $content->body($this->form()->edit($id));
+        });
     }
-
     /**
      * Create interface.
      *
-     * @param Content $content
      * @return Content
      */
-    public function create(Content $content)
+    public function create()
     {
-        return $content
-            ->header('Create')
-            ->description('description')
-            ->body($this->form());
+        return Admin::content(function (Content $content) {
+            $content->header('创建商品');
+            $content->body($this->form());
+        });
     }
-
     /**
      * Make a grid builder.
      *
@@ -90,7 +64,6 @@ class ProductsController extends Controller
             $grid->rating('评分');
             $grid->sold_count('销量');
             $grid->review_count('评论数');
-
             $grid->actions(function ($actions) {
                 $actions->disableView();
                 $actions->disableDelete();
@@ -103,32 +76,6 @@ class ProductsController extends Controller
             });
         });
     }
-
-    /**
-     * Make a show builder.
-     *
-     * @param mixed   $id
-     * @return Show
-     */
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->id('Id');
-        $show->title('Title');
-        $show->description('Description');
-        $show->image('Image');
-        $show->on_sale('On sale');
-        $show->rating('Rating');
-        $show->sold_count('Sold count');
-        $show->review_count('Review count');
-        $show->price('Price');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
-
     /**
      * Make a form builder.
      *
@@ -136,17 +83,27 @@ class ProductsController extends Controller
      */
     protected function form()
     {
-        $form = new Form(new Product);
-
-        $form->text('title', 'Title');
-        $form->textarea('description', 'Description');
-        $form->image('image', 'Image');
-        $form->switch('on_sale', 'On sale')->default(1);
-        $form->decimal('rating', 'Rating')->default(5.00);
-        $form->number('sold_count', 'Sold count');
-        $form->number('review_count', 'Review count');
-        $form->decimal('price', 'Price');
-
-        return $form;
+        // 创建一个表单
+        return Admin::form(Product::class, function (Form $form) {
+            // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
+            $form->text('title', '商品名称')->rules('required');
+            // 创建一个选择图片的框
+            $form->image('image', '封面图片')->rules('required|image');
+            // 创建一个富文本编辑器
+            $form->editor('description', '商品描述')->rules('required');
+            // 创建一组单选框
+            $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+            // 直接添加一对多的关联模型
+            $form->hasMany('skus', function (Form\NestedForm $form) {
+                $form->text('title', 'SKU 名称')->rules('required');
+                $form->text('description', 'SKU 描述')->rules('required');
+                $form->text('price', '单价')->rules('required|numeric|min:0.01');
+                $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+            });
+            // 定义事件回调，当模型即将保存时会触发这个回调
+            $form->saving(function (Form $form) {
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+            });
+        });
     }
 }
