3ddc0ceaf0a9ca4f7e07bb6cc63f7b23a5d3f51c
jinhesui <jinhesui@163.com>
2018-11-14 06:01:56 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
