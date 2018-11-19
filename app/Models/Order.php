57988d337e7f700f93168beca49cd904391a8896
jinhesui <jinhesui@163.com>
2018-11-19 14:58:19 +0000

    管理后台 - 优惠券列表

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -82,6 +82,11 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function couponCode()
+    {
+        return $this->belongsTo(CouponCode::class);
+    }
+
     public function items()
     {
         return $this->hasMany(OrderItem::class);
