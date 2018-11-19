929bda58b1b7f26bcda8ec0a6a6bbb167d374e0e
jinhesui <jinhesui@163.com>
2018-11-19 08:23:22 +0000

    微信支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,6 +43,7 @@ class AppServiceProvider extends ServiceProvider
 
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/1k7cq0h1';
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
