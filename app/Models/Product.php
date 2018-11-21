e3d530a7f6fea06a74add353fa681e1e3d24f4c4
jinhesui <jinhesui@163.com>
2018-11-21 06:53:22 +0000

    商品属性模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,6 +26,11 @@ class Product extends Model
         return $this->hasMany(ProductSku::class);
     }
 
+    public function properties()
+    {
+        return $this->hasMany(ProductProperty::class);
+    }
+
     public function crowdfunding()
     {
         return $this->hasOne(CrowdfundingProduct::class);
@@ -43,5 +48,16 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+    }
+
+    public function getGroupedPropertiesAttribute()
+    {
+        return $this->properties
+            // 按照属性名聚合，返回的集合的 key 是属性名，value 是包含该属性名的所有属性集合
+            ->groupBy('name')
+            ->map(function ($properties) {
+                // 使用 map 方法将属性集合变为属性值集合
+                return $properties->pluck('value')->all();
+            });
     }
 }
