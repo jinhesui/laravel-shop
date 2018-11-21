7341f6a927cd9077ea4033e4bb2e3972172f7017
jinhesui <jinhesui@163.com>
2018-11-21 04:36:39 +0000

    前台分期付款详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,6 +8,8 @@ use App\Models\UserAddress;
 use App\Policies\UserAddressPolicy;
 use App\Models\Order;
 use App\Policies\OrderPolicy;
+use App\Models\Installment;
+use App\Policies\InstallmentPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         UserAddress::class => UserAddressPolicy::class,
         Order::class       => OrderPolicy::class,
+        Installment::class => InstallmentPolicy::class,
     ];
 
     /**
