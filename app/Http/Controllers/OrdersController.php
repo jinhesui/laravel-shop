799ac7995b33c8ac4fe84ae5c6e0166722e97bd7
jinhesui <jinhesui@163.com>
2018-11-29 10:43:52 +0000

    秒杀商品详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -15,6 +15,7 @@ use App\Exceptions\CouponCodeUnavailableException;
 use App\Models\CouponCode;
 use App\Http\Requests\CrowdFundingOrderRequest;
 use App\Models\ProductSku;
+use App\Http\Requests\SeckillOrderRequest;
 
 class OrdersController extends Controller
 {
@@ -151,5 +152,14 @@ class OrdersController extends Controller
         $amount  = $request->input('amount');
 
         return $orderService->crowdfunding($user, $address, $sku, $amount);
+    }
+
+     public function seckill(SeckillOrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
+        $sku     = ProductSku::find($request->input('sku_id'));
+
+        return $orderService->seckill($user, $address, $sku);
     }
 }
